5382aae6ca2dd74241036d3f0764be338157e9d0
Bram Vlerick <bram.vlerick@barco.com>
2017-05-03 14:26:56 +0200

    Initial commit

---
+++ install-programs.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+OS="Invalid"
+
+function check_root {
+	if [ $(id -u) != "0" ]; then
+		echo "This script must be run as root"
+		exit 1
+	fi
+}
+
+function get_os {
+	echo "Installing Programs"
+	echo "Checking OS"
+	dnf 2> /dev/null
+	if [ $? -eq 1 ] 
+	then
+		echo "Fedora detected";
+		OS="Fedora"
+	fi
+
+	apt-get 2> /dev/null
+	if [ $? -eq 1 ] 
+	then
+		echo "Ubuntu detected";
+		OS="Ubuntu"
+	fi
+
+	pacman 2> /dev/null
+	if [ $? -eq 1 ]
+	then
+		echo "Arch Linux detected";
+		OS="Arch"
+	fi
+	
+	if [ $OS == "Invalid" ]
+	then
+		echo "Couldn't detect OS"
+		exit
+	fi
+}
+
+function install_programs {
+	if [ $OS == "Arch" ]
+	then
+		pacman -S --noconfirm qemu qemu-arch-extra libvirt git make cmake curl
+	fi
+}
+
+#retrieve OS
+check_root
+get_os
+install_programs
